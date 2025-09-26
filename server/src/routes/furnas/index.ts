db3455c39fd1028b414dde5b540d531122f66a65
Leo-Slv <leonardo1692004@gmail.com>
2025-09-26 20:44:57 -0300

    Conclusão task Implementar endpoint GET /furnas/bolhas→ BDR 045

--- server/src/routes/furnas/index.ts
+++ server/src/routes/furnas/index.ts
@@ -4,6 +4,7 @@ import campanha from "./campanha.routes";
 import instituicao from "./instituicao.routes";
 import reservatorio from "./reservatorio.routes";
 import sitio from "./sitio.routes";
+import bolha from "./bolhas.routes";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use("/campanha", campanha);
 router.use("/instituicao", instituicao);
 router.use("/reservatorio", reservatorio);
 router.use("/sitio", sitio);
+router.use("/bolhas", bolha)
 
 export default router;
