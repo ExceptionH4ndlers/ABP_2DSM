04e130e133db4a719c7363b875266aad21748817
Leo-Slv <leonardo1692004@gmail.com>
2025-10-02 20:09:41 -0300

    Implementar endpoint GET /balcar/tabelacampo/all → DWII 032

--- server/src/routes/balcar/index.ts
+++ server/src/routes/balcar/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import reservatorio from "./reservatorio.routes";
 import instituicao from "./instituicao.routes";
+import tabelacampo from "./tabelacampo.routes";
 
 const router = express.Router();
 
 router.use("/reservatorio", reservatorio);
 router.use("/instituicao", instituicao);
+router.use("/tabelacampo", tabelacampo);
 
 export default router;
